ea19a3286356edb63b5eaf8b53b45caa158327cf
Marine <marine_ponomarenko@mail.ru>
2023-07-06 18:51:08 +0300

    home work

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Console.WriteLine("Введите число");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int numberB = Convert.ToInt32(Console.ReadLine());  
+//   int max = 0;
+//   int min = 0;  
+
+// if (numberA > numberB) max = numberA;  
+// if (numberA > numberB) min = numberB;  
+  
+// if (numberA < numberB) max = numberB;  
+// if (numberA < numberB) min = numberA;
+
+// Console.WriteLine("Максимальное и минимальное число :"); 
+// Console.WriteLine(max);  
+// Console.WriteLine(min);
+  
+
+// Console.WriteLine("Введите число");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int numberC = Convert.ToInt32(Console.ReadLine()); 
+// int max = 0;  
+
+// if(numberA > numberB) max = numberA;  
+// if(numberA < numberB) max = numberB;  
+// if(numberA > numberC) max = numberA;  
+// if(numberA < numberC) max = numberC;  
+// if(numberB < numberC) max = numberC;  
+// if(numberB > numberC) max = numberB;  
+// {
+// Console.WriteLine("Максимальное число :");
+// }  
+// {
+// Console.WriteLine(max);
+// }
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());  
+// if (number % 2 == 0)  
+// {
+//     Console.WriteLine("Число четное");
+// }  
+// else
+// {
+//     Console.WriteLine("Число не чеетное");
+// }
+
+ 
