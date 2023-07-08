055225745df30032ff105e25e81581e08c3ab28c
Marine <marine_ponomarenko@mail.ru>
2023-07-08 15:13:15 +0300

    home work0707

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// ДЗ.07.07.2023. Семинар2
+
+// 1. Задача
+// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+// Console.WriteLine("Введите число :");
+// int number = Convert.ToInt32(Console.ReadLine());  
+
+// int result = (number / 10) % 10;  
+// Console.WriteLine(result);
+
+
+
+// 2. Задача
+// Напишите программу, которая выводит третью цифру
+//  заданного числа или сообщает, что третьей цифры нет.
+
+// Console.WriteLine("Введите трехзначное число :");  
+// int number = Convert.ToInt32(Console.ReadLine());  
+// int num = number % 10; 
+
+//  if (num == 0)
+//  Console.WriteLine("Повторите попытку");
+
+// else   
+// Console.WriteLine(num);
+
+// PS.(как написать программу так,чтобы это работало и с 
+// большими цыфрами , или как ограничить кол-во вводных
+// цыфр пользователем?)
+
+
+
+// 3. Задача
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли
+//  этот день выходным.
+
+// Console.WriteLine("Введите число :");
+// int number = Convert.ToInt32(Console.ReadLine());  
+// if (number == 1)  
+// Console.WriteLine("Понедельник");  
+
+// else if (number == 2)
+// Console.WriteLine("Вторник");  
+
+// else if (number == 3)  
+// Console.WriteLine("Среда");  
+
+// else if (number == 4)
+// Console.WriteLine("Четверг");  
+
+// else if (number ==5)
+// Console.WriteLine("Пятница");  
+
+// else if (number == 6)
+// Console.WriteLine("Суббота -  Ура,выходной ");  
+
+// else if (number == 7)
+// Console.WriteLine("Воскресенье - Ура, выходной");  
+
+// else 
+// Console.WriteLine("Такого дня недели нет");
